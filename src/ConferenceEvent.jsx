3bf9dc0560cecec93b347a241a8f7271ce7e016e
Geloscht <sg.tylerize@gmail.com>
2025-07-07 11:06:15 -0400

    changes made

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -7,9 +7,9 @@ const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => StorageEvent.av);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
-
     
     const handleToggleItems = () => {
         console.log("handleToggleItems called");
@@ -157,7 +157,20 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
-
+                                {avItems.map((item, index) => (
+                                    <div className="av_data venue_main" key={index}>
+                                        <div className="img">
+                                            <img src={item.img} alt={item.anme} />
+                                        </div>
+                                    <div className="text"> {item.name} </div>
+                                    <div> ${item.cost} </div>
+                                    <div className="addons_btn">
+                                        <button className="btn_warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                        <span className="quantity_value">{item.quantity}</span>
+                                        <button className="btn_success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                                    </div>
+                                    </div>
+                                ))}
                                 </div>
                                 <div className="total_cost">Total Cost:</div>
 
